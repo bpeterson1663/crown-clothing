56312a5b7ec61a50274b08c129128e10590a7dfd
Brady Peterson <bpeterson1663@gmail.com>
2021-05-06 07:29:27 -0500

    feat(menu item): add hover effect to menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types'
 import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subitlte">SHOP NOW</span>
     </div>
   </div>
