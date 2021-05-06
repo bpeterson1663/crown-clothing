41c109720acc3ca8d178319033854f83f2151831
Brady Peterson <bpeterson1663@gmail.com>
2021-05-06 07:05:46 -0500

    refactor(homepage): refactor homepage to separate components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './menu-item.styles.scss'
+
+const MenuItem = ({title, imageUrl, size}) => (
+    <div 
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        className={`${size} menu-item`}>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subitlte">SHOP NOW</span>
+        </div>
+    </div>
+)
+
+MenuItem.propTypes = {
+    title: PropTypes.string,
+    imageUrl: PropTypes.string,
+    size: PropTypes.string
+}
+
+export default MenuItem
