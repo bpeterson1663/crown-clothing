e8ea32fac515b52c5c66f53b542c8a3230aa8afd
Brady Peterson <bpeterson1663@gmail.com>
2021-05-06 08:49:57 -0500

    feat(header): create and implement header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom'
-
+import Header from './components/header/header.component'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/shop" component={ShopPage} />
         <Route path="/" component={HomePage} />
