83a679e77bf3c7fdc44a5abf473ffe940f92cd04
Brady Peterson <bpeterson1663@gmail.com>
2021-05-06 08:40:12 -0500

    feat(shop): create collection item and preview components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component'
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((_, idx) => idx < 4)
+        .map(({id, ...itemProps}) => (
+          <CollectionItem key={id} {...itemProps} />
+        ))}
+    </div>
+  </div>
+)
+
+export default CollectionPreview
