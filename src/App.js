8d784f43292c336e316e174494e712f4b2668603
Brady Peterson <bpeterson1663@gmail.com>
2021-05-06 07:51:28 -0500

    feat(router): install and implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   )
 }
