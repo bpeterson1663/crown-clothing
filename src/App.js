8d8a32b9b5d43ce9c22c7c1fe27200881ea3bcd5
Brady Peterson <bpeterson1663@gmail.com>
2021-05-07 08:33:07 -0500

    feat(authentication): create sign in and sign up page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 function App() {
   return (
     <div>
       <Header />
       <Switch>
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
         <Route path="/" component={HomePage} />
       </Switch>
     </div>
