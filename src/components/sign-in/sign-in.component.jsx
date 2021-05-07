a17eb6c3c91bf268e865cb2cc74d225d50a3024a
Brady Peterson <bpeterson1663@gmail.com>
2021-05-07 08:32:41 -0500

    feat(custom button): implement custom button in sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import FormInput from '../../components/form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 import './sign-in.styles.scss'
 
 class SignIn extends Component {
@@ -44,7 +45,7 @@ class SignIn extends Component {
             value={this.state.password}
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     )
