ad71bc035d45192a9f2229da952559ee41b11e69
Brady Peterson <bpeterson1663@gmail.com>
2021-05-09 17:35:08 -0500

    style(prettier): ran prettier

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,7 +5,7 @@ import './form-input.styles.scss'
 const FormInput = ({ handleChange, label, value, ...otherProps }) => (
   <div className="group">
     <input className="form-input" onChange={handleChange} {...otherProps} />
-    {label ? (<label className={`${value.length ? 'shrink' : ''} form-input-label`}>{label}</label>) : null}
+    {label ? <label className={`${value.length ? 'shrink' : ''} form-input-label`}>{label}</label> : null}
   </div>
 )
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,19 +3,19 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: process.env.REACT_APP_STAGE_API_KEY,
-    authDomain: process.env.REACT_APP_STAGE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_STAGE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STAGE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_STAGE_EMAIL_JS,
-    appId: process.env.REACT_APP_STAGE_APP_ID
-  }
+  apiKey: process.env.REACT_APP_STAGE_API_KEY,
+  authDomain: process.env.REACT_APP_STAGE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_STAGE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STAGE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_STAGE_EMAIL_JS,
+  appId: process.env.REACT_APP_STAGE_APP_ID,
+}
 
-  firebase.initializeApp(config)
+firebase.initializeApp(config)
 
-  export const auth = firebase.auth()
-  export const firestore = firebase.firestore()
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
 
-  const provider = new firebase.auth.GoogleAuthProvider()
-  provider.setCustomParameters({ prompt: 'select_account '})
-  export const signInWithGoogle = () => auth.signInWithPopup(provider)
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account ' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
