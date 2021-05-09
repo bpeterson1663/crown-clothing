7fe214393b8975dbae89e3d6b5bb6bcf62e0041a
Brady Peterson <bpeterson1663@gmail.com>
2021-05-08 21:57:19 -0500

    feat(firebase): implement authentication with google

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+import React, { Component } from 'react'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-        <Route path="/" component={HomePage} />
-      </Switch>
-    </div>
-  )
+import { auth } from './firebase/firebase.utils'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+
+  }
+  unsubscribeFromAuth = null
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </div>
+    )
+  }
+  
 }
 
 export default App
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import FormInput from '../../components/form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 import './sign-in.styles.scss'
 
 class SignIn extends Component {
@@ -46,6 +47,7 @@ class SignIn extends Component {
             required
           />
           <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
         </form>
       </div>
     )
