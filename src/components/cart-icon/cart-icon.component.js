380c2e831293405a2f162f827c5278c85e8af42d
Brady Peterson <bpeterson1663@gmail.com>
2021-05-13 07:43:14 -0500

    feat(cart): update quantity in bag

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -4,15 +4,23 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
-  </div>
-)
+const CartIcon = ({ toggleCartHidden, cartItems }) => {
+  const quantity = cartItems.reduce((acc, item) => (acc += item.quantity), 0)
+
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{quantity}</span>
+    </div>
+  )
+}
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+})
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
