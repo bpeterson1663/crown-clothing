5bc1602af2ccc50f64c07b4efbbcac647c89d1a9
Brady Peterson <bpeterson1663@gmail.com>
2021-05-18 22:10:47 -0500

    style(prettier): ran prettier

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -4,21 +4,19 @@ import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {
-      cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem}/>
-      ))
-    }
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-  cartItems
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
 })
 
 export default connect(mapStateToProps)(CartDropdown)
--- src/components/cart-item/cart-item.component.js
+++ src/components/cart-item/cart-item.component.js
@@ -2,17 +2,16 @@ import React from 'react'
 
 import './cart-item.styles.scss'
 
-const CartItem = ({ item: { imageUrl, price, name, quantity}}) => (
-    <div className="cart-item">
-        <img src={imageUrl} alt="item" />
-        <div className="item-details">
-            <span className="name">{name}</span>
-            <span className="price">
-                ${price} X {quantity}
-            </span>
-
-        </div>
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        ${price} X {quantity}
+      </span>
     </div>
+  </div>
 )
 
 export default CartItem
