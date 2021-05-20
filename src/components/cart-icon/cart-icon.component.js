444ed92834f101ee19ae8505e8d7574e49464520
Brady Peterson <bpeterson1663@gmail.com>
2021-05-19 22:06:04 -0500

    style(prettier): ran prettier

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
-import { selectCartItemsCount} from '../../redux/cart/cart.selectors'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
-
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
@@ -16,7 +15,7 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
 }
 
 const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state)
+  itemCount: selectCartItemsCount(state),
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,13 +1,9 @@
 import { createSelector } from 'reselect'
 
-const selectCart = state => state.cart;
+const selectCart = (state) => state.cart
 
-export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems
-)
+export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems)
 
-export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => cartItems.reduce((acc, item) => (acc += item.quantity), 0)
+export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((acc, item) => (acc += item.quantity), 0),
 )
