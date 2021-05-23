80c3d6d92e0f2eeb94ac5c239f78bc29caaad945
Brady Peterson <bpeterson1663@gmail.com>
2021-05-22 21:54:18 -0500

    feat(checkout): increase or decrease quantity at checkout

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { clearItemFromCart } from '../../redux/cart/cart.actions'
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
 import './checkout-item.styles.scss'
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, quantity, price, imageUrl } = cartItem
 
   return (
@@ -12,7 +12,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img alt="item" src={imageUrl} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <div className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </div>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -23,6 +31,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 })
 
 export default connect(null, mapDispatchToProps)(CheckoutItem)
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 const INITIAL_STATE = {
   hidden: true,
   cartItems: [],
@@ -21,6 +21,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id),
+      }
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       }
     default:
       return {
