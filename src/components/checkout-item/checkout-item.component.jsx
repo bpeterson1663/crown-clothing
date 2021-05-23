b9d1474855919536238ab06658a713d35949445f
Brady Peterson <bpeterson1663@gmail.com>
2021-05-22 21:26:54 -0500

    feact(checkout): create checkout item component

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './checkout-item.styles.scss'
+
+const CheckoutItem = ({ cartItem: { name, quantity, price, imageUrl } }) => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img alt="item" src={imageUrl} />
+    </div>
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
+    <div className="remove-button">&#10005;</div>
+  </div>
+)
+
+export default CheckoutItem
