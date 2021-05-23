d830e05b438e83939c1024e77409afc79045d4ee
Brady Peterson <bpeterson1663@gmail.com>
2021-05-22 21:28:27 -0500

    feat(checkout): implement cart item component in checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,11 +4,12 @@ const selectCart = (state) => state.cart
 
 export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems)
 
-export const selectCartHidden = createSelector(
-    [selectCart],
-    cart => cart.hidden
-)
+export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden)
 
 export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce((acc, item) => (acc += item.quantity), 0),
+)
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0),
 )
