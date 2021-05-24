c948e7b04ad1158d75318fe7d9cdc6978daac2c1
Brady Peterson <bpeterson1663@gmail.com>
2021-05-23 22:18:36 -0500

    feat(checkout): persist cart items using redux persist

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -28,9 +28,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: removeItemFromCart(state.cartItems, action.payload),
       }
     default:
-      return {
-        ...state,
-      }
+      return state
   }
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
-import rootReducer from './root-reducer'
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
 
-const middlewares = [logger]
+import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const middlewares = [logger];
 
-export default store
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
