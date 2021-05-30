4233073a3496f46e7a512e689f26a3436ce1d347
Brady Peterson <bpeterson1663@gmail.com>
2021-05-29 20:53:48 -0500

    style(prettier): ran prettier

--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 const HomePageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
 `
 
 export default HomePageContainer
