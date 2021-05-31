6b540350e38da621219594b3f0d35f83aa44485a
Brady Peterson <bpeterson1663@gmail.com>
2021-05-31 13:57:17 -0500

    refactor(cart): refactor css to styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-
-import './cart-item.styles.scss'
-
+import { CartItemContainer, CartImage, ItemDetails, Name } from './cart-item.styles'
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <CartItemContainer>
+    <CartImage src={imageUrl} alt="item" />
+    <ItemDetails>
+      <Name>{name}</Name>
+      <span>
         ${price} X {quantity}
       </span>
-    </div>
-  </div>
+    </ItemDetails>
+  </CartItemContainer>
 )
 
 export default CartItem
