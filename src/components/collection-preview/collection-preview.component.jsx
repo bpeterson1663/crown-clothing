6e66934c702da5722b4a8c6d0224c9cb38b51b70
Brady Peterson <bpeterson1663@gmail.com>
2021-06-04 21:36:14 -0500

    style(prettier): ran prettier

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CollectionPreviewContainer, Title, Preview} from './collection-preview.styles'
+import { CollectionPreviewContainer, Title, Preview } from './collection-preview.styles'
 import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({ title, items }) => (
--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const CollectionPreviewContainer = styled.h1`
   display: flex;
-    flex-direction: column;
-    margin-bottom: 30px;
+  flex-direction: column;
+  margin-bottom: 30px;
 `
 
 export const Title = styled.div`
-    font-size: 28px;
-    margin-bottom: 25px;
+  font-size: 28px;
+  margin-bottom: 25px;
 `
 
 export const Preview = styled.div`
