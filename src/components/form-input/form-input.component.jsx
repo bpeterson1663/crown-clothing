871c11706e528d58e082d40607d5a2070aa3cce3
Brady Peterson <bpeterson1663@gmail.com>
2021-06-09 23:06:09 -0500

    refactor(form): refactor sass to styled components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import { GroupContainer, FormInputContainer, FormInputLabel } from './form-input.styles'
 import './form-input.styles.scss'
 
 const FormInput = ({ handleChange, label, value, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
-    {label ? <label className={`${value.length ? 'shrink' : ''} form-input-label`}>{label}</label> : null}
-  </div>
+  <GroupContainer>
+    <FormInputContainer onChange={handleChange} {...otherProps} />
+    {label ? (
+      <FormInputLabel className={`${value.length ? 'shrink' : ''} form-input-label`}>{label}</FormInputLabel>
+    ) : null}
+  </GroupContainer>
 )
 
 export default FormInput
