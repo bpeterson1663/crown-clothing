c1cae04faddcbc454aab4f63af67adfe040d10c0
Brady Peterson <bpeterson1663@gmail.com>
2021-06-28 05:53:31 -0500

    feat(server): add stripe routes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-if (process.env.NODE_ENV === 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', function(req, res) {
@@ -19,6 +20,22 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port, error => {
-    if(error) throw error
+    if (error) throw error
     console.log('Server running on port ' + port)
+})
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({error: stripeErr })
+        } else {
+            res.status(200).send({ success: stripeRes })
+        }
+    })
 })
