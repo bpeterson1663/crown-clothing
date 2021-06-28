fd81356dc1821122665bb6e9352da6228a1fe783
Brady Peterson <bpeterson1663@gmail.com>
2021-06-28 06:27:44 -0500

    chore(env var): update env var names

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.REACT_APP_STAGE_API_KEY,
-  authDomain: process.env.REACT_APP_STAGE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_STAGE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STAGE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_STAGE_EMAIL_JS,
-  appId: process.env.REACT_APP_STAGE_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
