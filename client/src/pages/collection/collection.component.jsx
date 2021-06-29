9a320b5260a604329b09180d21955c85ab6fdbcf
Brady Peterson <bpeterson1663@gmail.com>
2021-06-29 04:49:58 -0500

    feat(context): use context api instead of redux example

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -2,20 +2,32 @@ import React from 'react'
 import { connect } from 'react-redux'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 import { selectCollection } from '../../redux/shop/shop.selector'
-
+import CollectionsContext from '../../contexts/collections/collections.context'
 import './collection.styles.scss'
 
-const CollectonPage = ({ match, collection }) => {
-  const { title, items } = collection
+const CollectonPage = ({ match }) => {
+  // const { title, items } = collection
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {items.map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+    {
+      collections => {
+        const collection = collections[match.params.collectionId]
+        const { title, items } = collection
+        return (
+          <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        )
+      }
+    }
+      
+    </CollectionsContext.Consumer>
+   
   )
 }
 
