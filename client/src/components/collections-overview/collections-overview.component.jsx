ad389a4f249c7229fa54f8165846fffbfd3eb044
Brady Peterson <bpeterson1663@gmail.com>
2021-06-30 08:13:48 -0500

    refactor(collections): remove redux and use graphql

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 import CollectionPreview from '../collection-preview/collection-preview.component'
 import './collections-overview.styles.scss'
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector'
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -13,8 +10,4 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview,
-})
-
-export default connect(mapStateToProps)(CollectionsOverview)
+export default CollectionsOverview
--- client/src/components/collections-overview/collections-overview.container.jsx
+++ client/src/components/collections-overview/collections-overview.container.jsx
@@ -1,15 +1,31 @@
-import { connect } from 'react-redux'
-import { compose } from 'redux'
-import { createStructuredSelector } from 'reselect'
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selector'
-
-import WithSpinner from '../with-spinner/with-spinner.component'
+import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import CollectionsOverview from './collections-overview.component'
+import Spinner from '../spinner/spinner.component'
 
-const mapStateToProps = createStructuredSelector({
-  isLoading: selectIsCollectionFetching,
-})
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
 
-const CollectionsOverviewContainer = compose(connect(mapStateToProps), WithSpinner)(CollectionsOverview)
+const CollectionsOverviewContainer = () => (
+  <Query query={GET_COLLECTIONS}>
+    {({ loading, data }) => {
+      if (loading) return <Spinner />
+      return <CollectionsOverview collections={data.collections} />
+    }}
+  </Query>
+)
 
 export default CollectionsOverviewContainer
