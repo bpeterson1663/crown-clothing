902c2914a605e45a5cd7c0f564ee7c279cb3a5ce
Brady Peterson <bpeterson1663@gmail.com>
2021-07-01 07:21:46 -0500

    chore(comments): remove commented out code

--- client/src/pages/collection/collection.container.jsx
+++ client/src/pages/collection/collection.container.jsx
@@ -1,17 +1,3 @@
-// import { connect } from 'react-redux'
-// import { compose } from 'redux'
-// import { createStructuredSelector } from 'reselect'
-// import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selector'
-// import WithSpinner from '../../components/with-spinner/with-spinner.component'
-// import CollectionPage from './collection.component'
-
-// const mapStateToProps = createStructuredSelector({
-//   isLoading: (state) => !selectIsCollectionsLoaded(state),
-// })
-
-// const CollectionPageContainer = compose(connect(mapStateToProps), WithSpinner)(CollectionPage)
-
-// export default CollectionPageContainer
 import React from 'react'
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
