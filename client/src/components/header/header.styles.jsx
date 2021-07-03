dc6cd5197f72a9385f4c0e9587e1bde48f6f4787
Brady Peterson <bpeterson1663@gmail.com>
2021-07-02 21:56:45 -0500

    feat(mobile): create mobile response design for header and items

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -7,12 +7,23 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    margin-bottom: 20px;
+    padding: 10px;
+  }
 `
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    padding: 0;
+    width: 50px;
+  }
 `
 
 export const OptionsContainer = styled.div`
@@ -21,7 +32,11 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `
+
 export const OptionLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`${title}`)}>
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className="background-image"
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subitlte">SHOP NOW</span>
-    </div>
-  </div>
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle,
+} from './menu-item.styles'
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <MenuItemContainer size={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <BackgroundImageContainer className="background-image" imageUrl={imageUrl} />
+    <ContentContainer className="content">
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>
 )
-
-MenuItem.propTypes = {
-  title: PropTypes.string,
-  imageUrl: PropTypes.string,
-  size: PropTypes.string,
-}
 
 export default withRouter(MenuItem)
