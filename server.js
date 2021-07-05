1b18f525f1dda42f1400f5439618d1ee63a0b36a
Brady Peterson <bpeterson1663@gmail.com>
2021-07-04 21:23:49 -0500

    perf(gzip): add compression library for gzip

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const compression = require('compression')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -8,6 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
