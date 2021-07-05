14784b2160caac7c6358b46342a030b392074879
Brady Peterson <bpeterson1663@gmail.com>
2021-07-05 07:18:01 -0500

    chore(https): set trustProtoHeader to true in production

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', function(req, res) {
