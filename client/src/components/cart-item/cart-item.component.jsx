6efe9ed831ee8219285d2cecd66b082eddd8a409
Brady Peterson <bpeterson1663@gmail.com>
2021-07-04 20:59:22 -0500

    perf(cart item): memoize cart item to prevent unnecessary rerendering when adding to cart

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { CartItemContainer, CartImage, ItemDetails, Name } from './cart-item.styles'
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <CartItemContainer>
@@ -12,4 +12,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   </CartItemContainer>
 )
 
-export default CartItem
+export default memo(CartItem)
